e98552da324895b9fea84d310ec0b68461335ec2
Nobaltrital <nobaltrital123@gmail.com>
2025-08-16 21:40:47 +0545

    yeah
    fixed errors

--- LoggedIn.cs
+++ LoggedIn.cs
@@ -7,6 +7,6 @@ public record LoggedIn (string Username,string Password)
     public Claim[] ToClaims() =>
         [
         new Claim(ClaimTypes.Name, Username ),
-         new Claim("Password", Password )
+     
     ];
 }
